7622584c519e2ffe3f7e57896a2497a58d446f50
Rumit Tuladhar <rumitwashere@gmail.com>
2019-01-07 12:50:29 +0545

    animation done

---
+++ twitteranimate/App.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import { 
+  StyleSheet,
+   Text, 
+   View,
+   ScrollView ,
+   Image,
+   Animated
+  } from 'react-native';
+
+ HEADER_MAX_HEIGHT = 120
+ HEADER_MIN_HEIGHT = 70
+ PROFILE_IMAGE_MAX_HEIGHT = 80
+ PROFILE_IMAGE_MIN_HEIGHT = 40
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      scrollY : new Animated.Value(0)
+    }
+  }
+  render() {
+    const headerHeight = this.state.scrollY.interpolate({
+      inputRange: [0,HEADER_MAX_HEIGHT-HEADER_MIN_HEIGHT],
+      outputRange: [HEADER_MAX_HEIGHT,HEADER_MIN_HEIGHT],
+      extrapolate: 'clamp'
+    })
+    const profileImageHeight = this.state.scrollY.interpolate({
+      inputRange: [0,HEADER_MAX_HEIGHT-HEADER_MIN_HEIGHT],
+      outputRange: [PROFILE_IMAGE_MAX_HEIGHT,PROFILE_IMAGE_MIN_HEIGHT],
+      extrapolate: 'clamp'
+    })
+    const profileImageMarginTop = this.state.scrollY.interpolate({
+      inputRange: [0,HEADER_MAX_HEIGHT-HEADER_MIN_HEIGHT],
+      outputRange: [HEADER_MAX_HEIGHT-(PROFILE_IMAGE_MAX_HEIGHT/2),
+        HEADER_MAX_HEIGHT+5],
+      extrapolate: 'clamp'
+    })
+    const headerZindex = this.state.scrollY.interpolate({
+      inputRange: [0,HEADER_MAX_HEIGHT-HEADER_MIN_HEIGHT],
+      outputRange: [0,1],
+      extrapolate: 'clamp'
+    })
+    const headerTitleBottom = this.state.scrollY.interpolate({
+      inputRange: [0,HEADER_MAX_HEIGHT-HEADER_MIN_HEIGHT,
+        HEADER_MAX_HEIGHT-HEADER_MIN_HEIGHT+5+PROFILE_IMAGE_MIN_HEIGHT,
+        HEADER_MAX_HEIGHT-HEADER_MIN_HEIGHT+5+PROFILE_IMAGE_MIN_HEIGHT+ 26
+      ],
+      outputRange: [-20,-20,-20,-0],
+      extrapolate: 'clamp'
+    })
+    return (
+      <Animated.View style={{flex:1}}>
+        <Animated.View style={{
+          position: 'absolute',
+          top: 0,
+          left: 0 ,
+          right:0,
+          backgroundColor: 'lightskyblue',
+          height: headerHeight,
+          zIndex: headerZindex,
+          alignItems: 'center'
+        }}>
+        <Animated.View style={{
+          position:'absolute',
+          bottom:headerTitleBottom
+          }}>
+          <Text style={{
+            color:'white',
+            fontSize:14,
+            fontWeight:'bold'
+            
+            }}>Rumit Tuladhar</Text>
+        </Animated.View>
+        </Animated.View>
+        <ScrollView style={{ flex:1 }}
+        scrollEventThrottle={16}
+        onScroll={Animated.event(
+          [{nativeEvent:{contentOffset: {y: this.state.scrollY}}}]
+        )}
+        >
+          <Animated.View style={{
+            height: profileImageHeight,
+            width: profileImageHeight,
+            borderRadius: PROFILE_IMAGE_MAX_HEIGHT/2,
+            borderColor: 'white',
+            borderWidth: 3,
+            overFlow: 'hidden',
+            marginTop: profileImageMarginTop,
+            marginLeft: 10,
+            overflow: 'hidden'
+
+          }}>
+                <Image source={require('./assets/me.jpg')}
+                style={{ flex:1,width: null, height: null}}
+                ></Image>
+          </Animated.View>
+          <View>
+            <Text style={{
+              fontWeight:'bold',
+              fontSize:26,
+              paddingLeft:10
+              }}>
+              Rumit Tuladhar
+            </Text>
+          </View>
+              <View style={{height: 1000 }}></View>
+        </ScrollView>
+
+     
+
+      </Animated.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ twitteranimate/readme.md
@@ -0,0 +1,5 @@
+# This is an Expo App 
+## that can be view on the expo app after downloading it
+`npm install ` then `expo start` **
+** need to install expo first **
+** can be view in the mobile too
