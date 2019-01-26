da312696427a78253a184e98590d21298b28d63b
Rumit Tuladhar <rumitwashere@gmail.com>
2019-01-26 14:59:25 +0545

    finished ui pan

--- appleMusicPan/App.js
+++ appleMusicPan/App.js
@@ -17,16 +17,100 @@ import {Ionicons} from '@expo/vector-icons'
 
   
 export default class App extends React.Component {
+  state = {
+    isScrollEnabled:false,
+
+  }
   componentWillMount(){
+    this.scrollOffset = 0;
+
     this.animation = new Animated.ValueXY({ x:0 , y:SCREEN_HEIGHT - 90});
+
+    this.PanResponder = PanResponder.create({
+      onMoveShouldSetPanResponder: (evt, gestureState) => {
+
+        if ((this.state.isScrollEnabled && this.scrollOffset <= 0 && gestureState.dy > 0) || !this.state.isScrollEnabled && gestureState.dy < 0) {
+          return true
+        } else {
+          return false
+        }
+      },
+      onPanResponderGrant:(evt,gestureState)=>{
+        this.animation.extractOffset()
+      },
+      onPanResponderMove:(evt,gestureState)=>{
+          this.animation.setValue({ x:0, y: gestureState.dy })
+      },
+      onPanResponderRelease:(evt,gestureState)=>{
+        if(gestureState.moveY>SCREEN_HEIGHT -120)
+        {
+          Animated.spring(this.animation.y,{
+            toValue:0,
+            tension:1
+          }).start()
+        }
+        else if (gestureState.moveY < 120)
+        {
+          Animated.spring(this.animation.y,{
+            toValue:0,
+            tension:1
+          }).start()
+        }
+         else if(gestureState.dy < 0){
+            this.setState({isScrollEnabled : true})
+            Animated.spring(this.animation.y,{
+              toValue: -SCREEN_HEIGHT + 120,
+              tension: 1
+            }).start()
+          }
+          else if(gestureState.dy > 0){
+            this.setState({isScrollEnabled:false})
+            Animated.spring(this.animation.y,{
+              toValue: SCREEN_HEIGHT - 120,
+              tension: 1
+            }).start()
+          }
+      }
+    })
   }
   render() {
     const animatedHeight = {
       transform : this.animation.getTranslateTransform()
     }
+    animatedImageHeight = this.animation.y.interpolate({
+      inputRange:[0,SCREEN_HEIGHT - 90],
+      outputRange: [200, 32],
+      extrapolate: "clamp"
+    })
+    animatedSongTitleOpacity = this.animation.y.interpolate({
+      inputRange:[0, SCREEN_HEIGHT - 500 ,SCREEN_HEIGHT - 90],
+      outputRange: [0,0,1],
+      extrapolate: "clamp"
+    })
+    animatedmageMarginLeft = this.animation.y.interpolate({
+      inputRange:[0,SCREEN_HEIGHT - 90],
+      outputRange: [SCREEN_WIDTH/2 -100, 10],
+      extrapolate: "clamp"
+    })
+    animatedHeaderHeight = this.animation.y.interpolate({
+      inputRange:[0,SCREEN_HEIGHT - 90],
+      outputRange: [SCREEN_HEIGHT/2 , 90],
+      extrapolate: "clamp"
+    })
+    animatedSongDetailsOpacity = this.animation.y.interpolate({
+      inputRange:[0, SCREEN_HEIGHT - 500 ,SCREEN_HEIGHT - 90],
+      outputRange: [1,0,0],
+      extrapolate: "clamp"
+    })
+    animatedBackgroundColor = this.animation.y.interpolate({
+      inputRange:[0,SCREEN_HEIGHT - 90],
+      outputRange: ['rgba(0,0,0,0.5)','white'],
+      extrapolate: "clamp"
+    })
     return (
-      <Animated.View style={{flex:1,backgroundColor: 'white'}}>
+      <Animated.View style={{flex:1,backgroundColor: animatedBackgroundColor}}>
       <Animated.View
+      {... this.PanResponder.panHandlers}
       style={[animatedHeight,{
         position:'absolute',
         left:0,
@@ -36,27 +120,62 @@ export default class App extends React.Component {
         height: SCREEN_HEIGHT
       }]}
       >
+      <ScrollView 
+      scrollEnabled={this.state.isScrollEnabled}
+      scrollEventThrottle={16}
+      onScroll={event=>{
+        this.scrollOffset = event.nativeEvent.contentOffset.y
+      }}
+      >
+
+      
       <Animated.View
-      style={{ height: 80,borderTopWidth:1, borderTopColor: '#ebe5e5',
+      
+      style={{ height: animatedHeaderHeight,borderTopWidth:1, borderTopColor: '#ebe5e5',
     flexDirection: 'row',alignItems: 'center' }}
       >
       <View style={{flex:4,flexDirection:'row',alignItems:'center'}}>
-      <Animated.View style={{height:32,width:32,marginLeft:10}}>
+      <Animated.View style={{height: animatedImageHeight, width:animatedImageHeight, marginLeft:animatedmageMarginLeft}}>
       <Image style={{flex:1,width:null,height:null}}
       source={require('./assets/hilife.jpg')}
       />
       </Animated.View>
-     <Animated.Text style={{opacity: 1,fontSize: 18, paddingLeft: 10}}>Fantasies</Animated.Text>
+     <Animated.Text style={{opacity: animatedSongTitleOpacity,fontSize: 18, paddingLeft: 10}}>Fantasies</Animated.Text>
       </View> 
-      <Animated.View style={{opacity:1,flex:1,flexDirection:'row',justifyContent: 'space-around'}}>
-         <Ionicons name="md-pause" size={32}/>
+      <Animated.View style={{opacity:animatedSongTitleOpacity,flex:1,flexDirection:'row',justifyContent: 'space-around'}}>
+      <Ionicons name="md-pause" size={32}/>
       <Ionicons name="md-play" size={32}/>
       </Animated.View>
-
-     
-    
-      
       </Animated.View>
+      <Animated.View style={{height:animatedHeaderHeight,opacity:animatedSongDetailsOpacity}}>
+      <View style={{flex:1,alignItems:'center',justifyContent:'flex-end'}}>
+      <Text style={{fontWeight:'bold',fontSize:22}}> Song Name here</Text>
+      <Text style={{color:'#fa95ed',fontSize:18}}> HI LIFE</Text>
+      </View>
+      <View style={{height:40,width:SCREEN_WIDTH,alignItems:'center'}}>
+      <Slider
+                  style={{ width: 300 }}
+                  step={1}
+                  minimumValue={18}
+                  maximumValue={71}
+                  value={18}
+
+                />
+
+      </View>
+      <View style={{flex:2,flexDirection:'row',alignitems:'center',justifyContent:'space-around'}}>
+      <Ionicons name="md-rewind" size={40}/>
+      <Ionicons name="md-pause" size={50}/>
+      <Ionicons name="md-fastforward" size={40}/>
+      </View>
+      <View style={{flexDirection:'row',justifyContent:'space-between',paddingHorizontal:20,paddingBottom:20}}>
+      <Ionicons name="md-add" size={32} style={{ color: '#fa95ed'}} />
+      <Ionicons name="md-more" size={32} style={{ color: '#fa95ed'}} />
+
+      </View>
+      </Animated.View>
+      <View style={{ height: 1000 }}/>
+      </ScrollView>
       </Animated.View>
       </Animated.View>
       
